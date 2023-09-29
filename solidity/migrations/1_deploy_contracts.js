e5faf6c3c116dc9ff90113be4986dfbdfb627884
Oskar Petto <oskar.petto@iteratec.com>
2023-09-29 20:21:08 +0200

    remove unused contract
    Former-commit-id: 553f576756d3ab59596b995116f1ecbeb69461d0

--- solidity/migrations/1_deploy_contracts.js
+++ solidity/migrations/1_deploy_contracts.js
@@ -1,4 +1,3 @@
-const Pairing = artifacts.require("Pairing");
 const InstantiationVerifier = artifacts.require("InstantiationVerifier");
 const TransitionVerifier = artifacts.require("TransitionVerifier");
 const TerminationVerifier = artifacts.require("TerminationVerifier");
@@ -6,10 +5,6 @@ const InstanceManager = artifacts.require("InstanceManager");
 
 module.exports = function (deployer) {
   deployer.then(async () => {
-    await deployer.deploy(Pairing);
-    deployer.link(Pairing, InstantiationVerifier);
-    deployer.link(Pairing, TransitionVerifier);
-    deployer.link(Pairing, TerminationVerifier);
     await deployer.deploy(InstantiationVerifier);
     await deployer.deploy(TransitionVerifier);
     await deployer.deploy(TerminationVerifier);
